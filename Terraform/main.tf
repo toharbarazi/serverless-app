fa0d2001a8360f092a8295390677f18164470165
toharbarazi <tohar.barazi@nitaznim.tech>
2025-11-27 17:04:50 +0200

    s3 bucket

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,38 @@
-#main file
+provider "aws" {
+    region = var.aws_region
+}
+
+resource "aws_s3_bucket" "bellybrew_recipe_book" {
+  bucket = var.bucket_name
+
+  # הגדרת ניהול הגישה (ACL)
+  object_ownership = "BucketOwnerEnforced"
+
+  # חסימת גישה ציבורית
+  block_public_access {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+
+  # מצב גרסאות (Versioning) לא פעיל
+  versioning {
+    enabled = false
+  }
+
+  # הגדרת הצפנה
+  server_side_encryption_configuration {
+    rule {
+      # הצפנה עם מפתחות S3
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  # הגדרת מפתח ה-bucket
+  bucket_key_enabled = true
+}
+
+
---
+++ Terraform/var.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+    description = "AWS region to use"
+    type        = string
+    default     = "us-east-1" 
+}
+
+variable "bucket_name" {
+    description = "AWS region to use"
+    type        = string
+    default     = "bellybrewrecipebook" 
+}
+
